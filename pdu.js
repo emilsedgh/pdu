76852130b3f13e8be4d4404cbab89a6c2f12502c
Alexis LE GOFF <alexis.legoff@gmail.com>
2014-09-10 13:06:12 +0200

    Use of GSM 7bit alphabet for deconding instead of ASCII alphabet

--- pdu.js
+++ pdu.js
@@ -161,6 +161,8 @@ pduParser.deSwapNibbles = function(nibbles) {
 }
 
 pduParser.decode7Bit = function(code, count) {
+    var sevenbitdefault = new Array('@', '£', '$', '¥', 'è', 'é', 'ù', 'ì', 'ò', 'Ç', '\n', 'Ø', 'ø', '\r','Å', 'å','\u0394', '_', '\u03a6', '\u0393', '\u039b', '\u03a9', '\u03a0','\u03a8', '\u03a3', '\u0398', '\u039e','€', 'Æ', 'æ', 'ß', 'É', ' ', '!', '"', '#', '¤', '%', '&', '\'', '(', ')','*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7','8', '9', ':', ';', '<', '=', '>', '?', '¡', 'A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S','T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Ä', 'Ö', 'Ñ', 'Ü', '§', '¿', 'a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o','p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ä', 'ö', 'ñ','ü', 'à');
+
     //We are getting 'septeps'. We should decode them.
     var binary = '';
     for(var i = 0; i<code.length;i++)
@@ -184,8 +186,10 @@ pduParser.decode7Bit = function(code, count) {
 
     var ascii = '';
     for(i in bin)
-        ascii += String.fromCharCode(parseInt(bin[i], 2));
-
+        if(parseInt(bin[i], 2)){
+            ascii += sevenbitdefault[parseInt(bin[i], 2)];
+        }
+        else break;
     return ascii;
 }
 
